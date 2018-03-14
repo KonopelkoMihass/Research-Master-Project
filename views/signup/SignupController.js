d8ac885e4e571d4645fe3e21b86dc5fab6d435df
Mihass <mihapelko93@gmail.com>
2018-03-14 21:04:10 +0000

    disabled team UI element

--- views/signup/SignupController.js
+++ views/signup/SignupController.js
@@ -19,7 +19,7 @@ class SignupController
 	signup(e)
 	{
 		var email = document.getElementById("signup-email").value;
-		var teamName = document.getElementById("signup-team-name").value;
+		var teamName = "";
 		var name = document.getElementById("signup-name").value;
     	var surname = document.getElementById("signup-surname").value;
 		var noun = document.getElementById("signup-noun").value;
